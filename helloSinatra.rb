55033daabedcdb7266d291e2bc1f2718228e3fa2
Catherine Stark <catherine.stark@yale.edu>
2014-02-22 15:21:39 -0500

    removed extra file

--- helloSinatra.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-
-get '/' do
-	html = %{<%= 
-		['<iframe width="560" height="315" src="//www.youtube.com/embed/p1JPKLa-Ofc?autoplay=1" frameborder="0" allowfullscreen></iframe>', '<iframe width="560" height="315" src="//www.youtube.com/embed/3xUfCUFPL-8" frameborder="0" allowfullscreen></iframe>'].sample
-	 %>}
-	erb html
-	
-end
